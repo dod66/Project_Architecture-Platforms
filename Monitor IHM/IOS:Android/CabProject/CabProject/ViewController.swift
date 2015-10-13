30dff61cc233134a9b8b579cfbb5114eaa7ffc8c
ensangetor <Remi@FlyingMac.imerir.lan>
2015-10-13 15:05:30 +0200

    Commit 1 test - ToDelete

---
+++ Monitor IHM/IOS:Android/CabProject/CabProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CabProject
+//
+//  Created by Remi Gandou on 12/10/2015.
+//  Copyright © 2015 Gnd Industry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
