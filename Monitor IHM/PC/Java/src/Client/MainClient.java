f26cdff68966d8ac84e36892a8054efa2f914ef0
ensangetor <Remi@FlyingMac.imerir.lan>
2015-10-14 10:20:37 +0200

    Push du Java - Connexion à un srv ws

---
+++ Monitor IHM/PC/Java/src/Client/MainClient.java
@@ -0,0 +1,10 @@
+package Client;
+
+public class MainClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection connect = new Connection();
+		connect.run(); //Run server connection
+	}
+}
