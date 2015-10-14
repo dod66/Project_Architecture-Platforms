d6db71064c67a908bf7fe877f0f5afbe37ee9296
dod66 <dod.f@live.fr>
2015-10-14 16:01:10 +0200

    GalileoV1.2
    Connection en websocket

--- Galiléo/galileo/galileo.ino
+++ Galiléo/galileo/galileo.ino
@@ -1,9 +1,8 @@
-#include <WebSocketClient.h>
+#include "Arduino.h"
 #include <Ethernet.h>
 #include <SPI.h>
 #include <LiquidCrystal.h>
-
-
+#include <WebSocketClient.h>
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
@@ -15,9 +14,9 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 #define btnNONE   5
 
 byte mac[] = {0x98, 0x4F, 0xEE, 0x05, 0x37, 0xE8};
-IPAddress ip(192, 168, 1, 211);
-byte server[] = {192, 168, 1, 50};
-
+IPAddress ip(192, 168, 1, 234);
+//192.168.1.10
+//char server[] = "echo.websocket.org";
 int readKey = 0; 
 int Pin = 9;
 int key = 0;
@@ -33,46 +32,24 @@ int readButtons(){                                              //Structuration 
  return btnNONE;
 }
 
-EthernetClient client;
-WebSocketClient webSocketClient;
+WebSocketClient client;
 
 void setup() {
-  
-  Ethernet.begin(mac, ip);
-  
-  Serial.begin(9600); 
   pinMode(Pin, OUTPUT);
   lcd.begin(16, 2);
-  
+  Serial.begin(9600);
   Serial.println("connecting...");
-  int erreur = client.connect("192.168.1.50", 800);
-  
-  if (client.connected()) {
-    Serial.println("connected");
-    client.println("GET /diviceConnect HTTP/1.0");
-    client.println();
-  } else {
-    Serial.println("connection failed");
-  }
-  
+  Serial.println("EXAMPLE: setup()");
+  Ethernet.begin(mac, ip);
+  client.connect("192.168.1.50",2009);
+  client.onOpen(onOpen);
+  client.onMessage(onMessage);
+  client.onError(onError);
+  client.send("Hello World!");
 }
 
 void loop() {
-  
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    for(;;)
-      ;
-  }
-  
-  webSocketClient.monitor();
+  client.monitor();
   
   key = readButtons();
   
@@ -106,9 +83,19 @@ void loop() {
      Serial.write("S"); 
       delay(300);
     break;
-  }
+  }	
 }
 
-void dataArrived(WebSocketClient webSocketClient, String data) {
-  Serial.println("Data Arrived: " + data);
+void onOpen(WebSocketClient client) {
+  Serial.println("EXAMPLE: onOpen()");
+}
+
+void onMessage(WebSocketClient client, char* message) {
+  Serial.println("EXAMPLE: onMessage()");
+  Serial.print("Received: "); Serial.println(message);
+}
+
+void onError(WebSocketClient client, char* message) {
+  Serial.println("EXAMPLE: onError()");
+  Serial.print("ERROR: "); Serial.println(message);
 }
