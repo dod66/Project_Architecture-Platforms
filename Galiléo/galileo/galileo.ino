1f6b8cbf04e9dbedcf8ed665b27263fdff0a13e0
dod66 <dod.f@live.fr>
2015-10-15 16:06:24 +0200

    GalileoV1.5

--- Galiléo/galileo/galileo.ino
+++ Galiléo/galileo/galileo.ino
@@ -1,28 +1,27 @@
 #include <aJSON.h>
-
 #include "Arduino.h"
 #include <Ethernet.h>
 #include <SPI.h>
 #include <LiquidCrystal.h>
 #include <WebSocketClient.h>
-
+//initialisation des broches de l'écran lcd
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-
-#define btnRIGHT  0                                            //déclare les broches des boutons
+//déclare les broches des boutons
+#define btnRIGHT  0                                            
 #define btnUP     1
 #define btnDOWN   2
 #define btnLEFT   3
 #define btnSELECT 4
 #define btnNONE   5
-
+// adresse mac du port ethernet du Galileo
 byte mac[] = {0x98, 0x4F, 0xEE, 0x05, 0x37, 0xE8};
-IPAddress ip(192, 168, 1, 234);
+IPAddress ip(192, 168, 1, 215);
 //192.168.1.10
-//char server[] = "echo.websocket.org";
+
 int readKey = 0; 
 int Pin = 9;
 int key = 0;
-
+//initialisation des boutons
 int readButtons(){                                              //Structuration des boutons
  readKey = analogRead(0);
  if(readKey < 50)   return btnRIGHT;                          
@@ -33,21 +32,49 @@ int readButtons(){                                              //Structuration 
  
  return btnNONE;
 }
-
+//declaration du client http
+EthernetClient httpClient;
+//declaration du client websocket
 WebSocketClient client;
-
+//definition de la fonction
 char* parseJson(char *jsonString) ;
-
-// Json string to parse
-char jsonString[] = "{\"query\":{\"count\":1,\"created\":\"2012-08-04T14:46:03Z\",\"lang\":\"en-US\",\"results\":{\"item\":{\"title\":\"Handling FTP usernames with @ in them\"}}}}";
+//exemple trame json pour la distance
+char jsonString[] = "{\"rootObject\":{\"cabInfo\":{\"odometer\":\"20\",\"destination\":\"null\",\"loc_now\":\"test\",\"loc_prior\":\"test\"}}}";
 
 void setup() {
+  //initialisation de la broche de sortie
   pinMode(Pin, OUTPUT);
+  //definition de la taille du lcd 16 caracteres sur 2 lignes
   lcd.begin(16, 2);
+  lcd.print("Take passenger ?");
+  lcd.setCursor(4,2);
+  lcd.print("YES");
+  lcd.setCursor(10,2);
+  lcd.print("NO");
+  lcd.setCursor(3,2);
+  lcd.print(">");
+  //definition de la vitesse de la trame en 9600 bodes
   Serial.begin(9600);
   Serial.println("Connexion...");
-  
+  //initialise le port ethernet avec l adresse mac et une ip
   Ethernet.begin(mac, ip);
+  //connection au serveur http 
+  int erreur = httpClient.connect("192.168.1.10",5000);
+  //si la connection est reussite
+  if (httpClient.connected()) {
+    Serial.println("Connected");
+    //on envoie un get sur le serveur
+    httpClient.println("GET /deviceConnect HTTP/1.0");
+    httpClient.println();
+  } else {
+    Serial.println("Connection failed");
+  }
+  
+  if (httpClient.available()) {
+    char c = httpClient.read();
+    Serial.print(c);
+  }
+  //connection au serveur websocket
   client.connect("192.168.1.10",6000);
   client.onOpen(onOpen);
   client.onMessage(onMessage);
@@ -57,41 +84,38 @@ void setup() {
   
   char* value = parseJson(jsonString);
 
-    if (value) {
-        Serial.println("Successfully Parsed: ");
-        Serial.println(value);
-    } else {
-        Serial.println("There was some problem in parsing the JSON");
-    }
+  if (value) {
+      Serial.println("Successfully Parsed: ");
+      Serial.println(value);
+  } else {
+      Serial.println("There was some problem in parsing the JSON");
+  }
+  
 }
-
+//parse du json
 char* parseJson(char *jsonString) 
 {
     char* value;
 
-    aJsonObject* root = aJson.parse(jsonString);
+    aJsonObject* trame = aJson.parse(jsonString);
 
-    if (root != NULL) {
+    if (trame != NULL) {
         //Serial.println("Parsed successfully 1 " );
-        aJsonObject* query = aJson.getObjectItem(root, "query"); 
+        aJsonObject* rootObject = aJson.getObjectItem(trame, "rootObject"); 
 
-        if (query != NULL) {
+        if (rootObject != NULL) {
             //Serial.println("Parsed successfully 2 " );
-            aJsonObject* results = aJson.getObjectItem(query, "results"); 
+            aJsonObject* cabInfo = aJson.getObjectItem(rootObject, "cabInfo"); 
 
-            if (results != NULL) {
+            if (cabInfo != NULL) {
                 //Serial.println("Parsed successfully 3 " );
-                aJsonObject* item = aJson.getObjectItem(results, "item"); 
-
-                if (item != NULL) {
-                    //Serial.println("Parsed successfully 4 " );
-                    aJsonObject* title = aJson.getObjectItem(item, "title"); 
+                aJsonObject* odometer = aJson.getObjectItem(cabInfo, "odometer");
                     
-                    if (title != NULL) {
-                        //Serial.println("Parsed successfully 5 " );
-                        value = title->valuestring;
-                    }
+                if (odometer != NULL) {
+                    //Serial.println("Parsed successfully 5 " );
+                    value = odometer->valuestring;
                 }
+                
             }
         }
     }
@@ -104,6 +128,8 @@ char* parseJson(char *jsonString)
 }
 
 void loop() {
+  
+  
   client.monitor();
   
   serialEvent();
@@ -119,19 +145,36 @@ void loop() {
     
     case btnDOWN:
       lcd.clear();
-      Serial.write("D");  
+      Serial.write("D"); 
+      client.send("test");  
       delay(300);
     break;
     
     case btnRIGHT:
       lcd.clear(); 
       Serial.write("R");
+      lcd.clear();
+      lcd.print("Take passenger ?");
+      lcd.setCursor(4,2);
+      lcd.print("YES");
+      lcd.setCursor(10,2);
+      lcd.print("NO");
+      lcd.setCursor(9,2);
+      lcd.print(">"); 
       delay(300);
     break;
 
     case btnLEFT:
       lcd.clear();
-      Serial.write("L"); 
+      Serial.write("L");
+      lcd.clear();
+      lcd.print("Take passenger ?");
+      lcd.setCursor(4,2);
+      lcd.print("YES");
+      lcd.setCursor(10,2);
+      lcd.print("NO");
+      lcd.setCursor(3,2);
+      lcd.print(">"); 
       delay(300);
     break;
     
@@ -150,17 +193,17 @@ void serialEvent(){
     lcd.print(string);                                     //écrit sur l'écran lcd
   }
 }
-
+//fonction detection de connection sur websocket
 void onOpen(WebSocketClient client) {
   Serial.println("Connection etablie...");
 }
-
+//fonction reception de messages par websocket
 void onMessage(WebSocketClient client, char* message) {
   Serial.println("EXAMPLE: onMessage()");
   Serial.print("Received: "); Serial.println(message);
 }
-
+//fonction detection d erreurs connection websocket
 void onError(WebSocketClient client, char* message) {
-  //Serial.println("EXAMPLE: onError()");
+  Serial.println("EXAMPLE: onError()");
   Serial.print("ERROR: "); Serial.println(message);
 }
