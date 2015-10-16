85bc62b1d293b6b403c45c7a23116b2fe1214356
dod66 <dod.f@live.fr>
2015-10-16 11:48:10 +0200

    taxi acceptation + send json data

--- Galiléo/galileo/galileo.ino
+++ Galiléo/galileo/galileo.ino
@@ -14,7 +14,8 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 #define btnSELECT 4
 #define btnNONE   5
 // adresse mac du port ethernet du Galileo
-byte mac[] = {0x98, 0x4F, 0xEE, 0x05, 0x37, 0xE8};
+byte mac[] = {
+  0x98, 0x4F, 0xEE, 0x05, 0x37, 0xE8};
 IPAddress ip(192, 168, 1, 215);
 //192.168.1.10
 
@@ -23,14 +24,14 @@ int Pin = 9;
 int key = 0;
 //initialisation des boutons
 int readButtons(){                                              //Structuration des boutons
- readKey = analogRead(0);
- if(readKey < 50)   return btnRIGHT;                          
- if(readKey < 250)  return btnUP; 
- if(readKey < 450)  return btnDOWN;
- if(readKey < 650)  return btnLEFT;
- if(readKey < 850)  return btnSELECT;
- 
- return btnNONE;
+  readKey = analogRead(0);
+  if(readKey < 50)   return btnRIGHT;                          
+  if(readKey < 250)  return btnUP; 
+  if(readKey < 450)  return btnDOWN;
+  if(readKey < 650)  return btnLEFT;
+  if(readKey < 850)  return btnSELECT;
+
+  return btnNONE;
 }
 //declaration du client http
 EthernetClient httpClient;
@@ -39,20 +40,14 @@ WebSocketClient client;
 //definition de la fonction
 char* parseJson(char *jsonString) ;
 //exemple trame json pour la distance
-char jsonString[] = "{\"rootObject\":{\"cabInfo\":{\"odometer\":\"20\",\"destination\":\"null\",\"loc_now\":\"test\",\"loc_prior\":\"test\"}}}";
+char jsonString[] = "{\"rootObject\":{\"cabInfo\":{\"odometer\":\"20\",\"destination\":\"null\",\"loc_now\":\"test_pos_taxi\",\"loc_prior\":\"test_pos_cible\"}}}"; 
 
 void setup() {
   //initialisation de la broche de sortie
   pinMode(Pin, OUTPUT);
   //definition de la taille du lcd 16 caracteres sur 2 lignes
   lcd.begin(16, 2);
-  lcd.print("Take passenger ?");
-  lcd.setCursor(4,2);
-  lcd.print("YES");
-  lcd.setCursor(10,2);
-  lcd.print("NO");
-  lcd.setCursor(3,2);
-  lcd.print(">");
+  takePassengerYesLCD();
   //definition de la vitesse de la trame en 9600 bodes
   Serial.begin(9600);
   Serial.println("Connexion...");
@@ -66,10 +61,11 @@ void setup() {
     //on envoie un get sur le serveur
     httpClient.println("GET /deviceConnect HTTP/1.0");
     httpClient.println();
-  } else {
+  } 
+  else {
     Serial.println("Connection failed");
   }
-  
+
   if (httpClient.available()) {
     char c = httpClient.read();
     Serial.print(c);
@@ -79,109 +75,56 @@ void setup() {
   client.onOpen(onOpen);
   client.onMessage(onMessage);
   client.onError(onError);
-  
+
   Serial.println(jsonString);
-  
-  char* value = parseJson(jsonString);
 
-  if (value) {
-      Serial.println("Successfully Parsed: ");
-      Serial.println(value);
-  } else {
-      Serial.println("There was some problem in parsing the JSON");
-  }
-  
 }
-//parse du json
-char* parseJson(char *jsonString) 
-{
-    char* value;
 
-    aJsonObject* trame = aJson.parse(jsonString);
-
-    if (trame != NULL) {
-        //Serial.println("Parsed successfully 1 " );
-        aJsonObject* rootObject = aJson.getObjectItem(trame, "rootObject"); 
-
-        if (rootObject != NULL) {
-            //Serial.println("Parsed successfully 2 " );
-            aJsonObject* cabInfo = aJson.getObjectItem(rootObject, "cabInfo"); 
-
-            if (cabInfo != NULL) {
-                //Serial.println("Parsed successfully 3 " );
-                aJsonObject* odometer = aJson.getObjectItem(cabInfo, "odometer");
-                    
-                if (odometer != NULL) {
-                    //Serial.println("Parsed successfully 5 " );
-                    value = odometer->valuestring;
-                }
-                
-            }
-        }
-    }
-
-    if (value) {
-        return value;
-    } else {
-        return NULL;
-    }
-}
 
 void loop() {
-  
-  
+  int yesOrNo = 1;
+
   client.monitor();
-  
+
   serialEvent();
   key = readButtons();
-  
-   switch(key){                                                   //lecture des boutons
-    case btnUP:
-      lcd.clear();
-      Serial.write("U");
-      client.send("Salut");      
-      delay(300);
-    break;
-    
-    case btnDOWN:
-      lcd.clear();
-      Serial.write("D"); 
-      client.send("test");  
-      delay(300);
-    break;
-    
-    case btnRIGHT:
-      lcd.clear(); 
-      Serial.write("R");
-      lcd.clear();
-      lcd.print("Take passenger ?");
-      lcd.setCursor(4,2);
-      lcd.print("YES");
-      lcd.setCursor(10,2);
-      lcd.print("NO");
-      lcd.setCursor(9,2);
-      lcd.print(">"); 
-      delay(300);
+
+  switch(key){                                                   //lecture des boutons
+  case btnUP:
+    lcd.clear();
+    Serial.write("U");
+    client.send("Salut");      
+    delay(300);
     break;
 
-    case btnLEFT:
-      lcd.clear();
-      Serial.write("L");
-      lcd.clear();
-      lcd.print("Take passenger ?");
-      lcd.setCursor(4,2);
-      lcd.print("YES");
-      lcd.setCursor(10,2);
-      lcd.print("NO");
-      lcd.setCursor(3,2);
-      lcd.print(">"); 
-      delay(300);
+  case btnDOWN:
+    lcd.clear();
+    Serial.write("D"); 
+    client.send("test");  
+    delay(300);
     break;
-    
-    case btnSELECT:
-      lcd.clear();
-      Serial.write("S"); 
-      delay(300);
+
+  case btnRIGHT:
+    lcd.clear(); 
+    Serial.write("R");
+    takePassengerNoLCD();
+    yesOrNo = 1;
+    delay(300);
+    break;
+
+  case btnLEFT:
+    lcd.clear();
+    Serial.write("L");
+    takePassengerYesLCD();
+    yesOrNo = 2;
+    delay(300);
+    break;
+
+  case btnSELECT:
+    lcd.clear();
+    Serial.write("S");
+    signalTakePassenger(yesOrNo);
+    delay(300);
     break;
   }	
 }
@@ -193,6 +136,62 @@ void serialEvent(){
     lcd.print(string);                                     //écrit sur l'écran lcd
   }
 }
+//fonction detection yes ou no
+void signalTakePassenger(int yesOrNo) {
+  char *trame;
+  aJsonObject *root;
+  root = aJson.createObject();
+  //si on decide de prendre la course
+  if (yesOrNo == 1) {
+    aJson.addStringToObject(root, "takePassenger", "Yes");
+    trame = root->valuestring;
+    Serial.println(trame);
+    char* posTaxi = parseJsonTaxiPos(jsonString);
+    char* posCible = parseJsonTargetPos(jsonString);
+    Serial.println(posTaxi);
+    Serial.println(posCible);
+    //while (posCible != posTaxi) {
+     //char* posTaxi = parseJsonTaxiPos(jsonString);
+     char* distance = parseJsonOdometer(jsonString);
+     Serial.print(distance);
+    //}
+    
+    //client.send(trame);
+  }
+  //si on refuse de prendre la course
+  if (yesOrNo == 2) {
+    aJson.addStringToObject(root, "takePassenger", "No");
+    trame = root->valuestring;
+    aJson.print(root);
+    Serial.println(trame);
+    //client.send(trame);
+
+  } 
+}
+
+//fonction affichage
+void takePassengerYesLCD() {
+  lcd.clear();
+  lcd.print("Take passenger ?");
+  lcd.setCursor(4,2);
+  lcd.print("YES");
+  lcd.setCursor(10,2);
+  lcd.print("NO");
+  lcd.setCursor(3,2);
+  lcd.print(">");
+}
+
+void takePassengerNoLCD() {
+  lcd.clear();
+  lcd.print("Take passenger ?");
+  lcd.setCursor(4,2);
+  lcd.print("YES");
+  lcd.setCursor(10,2);
+  lcd.print("NO");
+  lcd.setCursor(9,2);
+  lcd.print(">");  
+}
+
 //fonction detection de connection sur websocket
 void onOpen(WebSocketClient client) {
   Serial.println("Connection etablie...");
@@ -200,10 +199,119 @@ void onOpen(WebSocketClient client) {
 //fonction reception de messages par websocket
 void onMessage(WebSocketClient client, char* message) {
   Serial.println("EXAMPLE: onMessage()");
-  Serial.print("Received: "); Serial.println(message);
+  Serial.print("Received: "); 
+  Serial.println(message);
 }
 //fonction detection d erreurs connection websocket
 void onError(WebSocketClient client, char* message) {
   Serial.println("EXAMPLE: onError()");
-  Serial.print("ERROR: "); Serial.println(message);
+  Serial.print("ERROR: "); 
+  Serial.println(message);
 }
+
+char* parseJsonOdometer(char *jsonString) {
+  char* value;
+
+  aJsonObject* trame = aJson.parse(jsonString);
+
+  if (trame != NULL) {
+    //Serial.println("Parsed successfully 1 " );
+    aJsonObject* rootObject = aJson.getObjectItem(trame, "rootObject"); 
+
+    if (rootObject != NULL) {
+      //Serial.println("Parsed successfully 2 " );
+      aJsonObject* cabInfo = aJson.getObjectItem(rootObject, "cabInfo"); 
+
+      if (cabInfo != NULL) {
+        //Serial.println("Parsed successfully 3 " );
+        aJsonObject* odometer = aJson.getObjectItem(cabInfo, "odometer");
+
+        if (odometer != NULL) {
+          //Serial.println("Parsed successfully 5 " );
+          value = odometer->valuestring;
+        }
+
+      }
+    }
+  }
+
+  if (value) {
+    return value;
+  } 
+  else {
+    return NULL;
+  }
+}
+
+char* parseJsonTaxiPos(char *jsonString) 
+{
+  char* value;
+
+  aJsonObject* trame = aJson.parse(jsonString);
+
+  if (trame != NULL) {
+    //Serial.println("Parsed successfully 1 " );
+    aJsonObject* rootObject = aJson.getObjectItem(trame, "rootObject"); 
+
+    if (rootObject != NULL) {
+      //Serial.println("Parsed successfully 2 " );
+      aJsonObject* cabInfo = aJson.getObjectItem(rootObject, "cabInfo"); 
+
+      if (cabInfo != NULL) {
+        //Serial.println("Parsed successfully 3 " );
+        aJsonObject* loc_now = aJson.getObjectItem(cabInfo, "loc_now");
+
+        if (loc_now != NULL) {
+          //Serial.println("Parsed successfully 5 " );
+          value = loc_now->valuestring;
+        }
+
+      }
+    }
+  }
+
+  if (value) {
+    return value;
+  } 
+  else {
+    return NULL;
+  }
+}
+
+char* parseJsonTargetPos(char *jsonString) 
+{
+  char* value;
+
+  aJsonObject* trame = aJson.parse(jsonString);
+
+  if (trame != NULL) {
+    //Serial.println("Parsed successfully 1 " );
+    aJsonObject* rootObject = aJson.getObjectItem(trame, "rootObject"); 
+
+    if (rootObject != NULL) {
+      //Serial.println("Parsed successfully 2 " );
+      aJsonObject* cabInfo = aJson.getObjectItem(rootObject, "cabInfo"); 
+
+      if (cabInfo != NULL) {
+        //Serial.println("Parsed successfully 3 " );
+        aJsonObject* loc_prior = aJson.getObjectItem(cabInfo, "loc_prior");
+
+        if (loc_prior != NULL) {
+          //Serial.println("Parsed successfully 5 " );
+          value = loc_prior->valuestring;
+        }
+
+      }
+    }
+  }
+
+  if (value) {
+    return value;
+  } 
+  else {
+    return NULL;
+  }
+}
+
+
+
