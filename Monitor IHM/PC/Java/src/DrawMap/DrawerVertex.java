2f6a7186e117a35683a66302ad9047b0c2afb8dd
dod66 <dod.f@live.fr>
2015-10-22 09:48:55 +0200

    DrawVertex with commentary

--- Monitor IHM/PC/Java/src/DrawMap/DrawerVertex.java
+++ Monitor IHM/PC/Java/src/DrawMap/DrawerVertex.java
@@ -3,44 +3,47 @@ package DrawMap;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
-
 import frameTreatment.Area;
 import frameTreatment.Vertex;
 
-
+//classe pour dessiner les vertex 
 public class DrawerVertex {
 
 	private int posX;
 	private int posY;
 	private float x, y;
 	private String name;
-	
-		public DrawerVertex(Area area, Graphics2D g) {
-			
-			for (Vertex vertex : area.getMap().getVertices()){
-				x = vertex.getX();
-				y = vertex.getY();
-				name = vertex.getName();
-				
-				buildVertex(name,x,y,g);		
-			}
+	//constructeur qui prend en parametre un objet Area et un Graphics
+	public DrawerVertex(Area area, Graphics2D g) {
+		System.out.println(area.getMap());
+		//pour tous les objets vertex contenu dans l Area
+		for (Vertex vertex : area.getMap().getVertices()){
+			//recuperation des coordonnees et du nom du vertex
+			x = vertex.getX();
+			y = vertex.getY();
+			name = vertex.getName();
+			//execute la fonction pour dessiner le vertex
+			buildVertex(name,x,y,g);		
 		}
-		
-		public void buildVertex(String name, float x, float y, Graphics2D g) {
-			
-			Dimension size = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-	        double width_frame = size.getWidth();
-	        double height_frame = size.getHeight();
-	        int width_frame_int = (int)width_frame;
-	        int height_frame_int = (int)height_frame;
-	        
-	        posX = (int) (x * width_frame_int);
-			posY = (int) (y * height_frame_int);
-			
-			System.out.println("Vertex " + name +  " :" + posX + ", " + posY);
-			
-			g.setColor(Color.blue);
-			g.drawRect(posX, posY, 20, 20);
-	        
-		}
+	}
+	//fonction pour dessiner le vertex
+	public void buildVertex(String name, float x, float y, Graphics2D g) {
+
+		//		Dimension size = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		//		double width_frame = size.getWidth();
+		//		double height_frame = size.getHeight();
+		//		int width_frame_int = (int)width_frame - 100;
+		//		int height_frame_int = (int)height_frame - 100;
+
+		//conversion des coordonnees en fonction de la taille de la fenetre
+		posX = (int) (x * 1340);
+		posY = (int) (y * 800);
+
+		System.out.println("Vertex " + name +  " :" + posX + ", " + posY);
+		//definition d une couleur
+		g.setColor(Color.BLUE);
+		//dessin du rectable representant un vertex
+		g.drawRect(posX, posY, 20, 20);
+
+	}
 }
